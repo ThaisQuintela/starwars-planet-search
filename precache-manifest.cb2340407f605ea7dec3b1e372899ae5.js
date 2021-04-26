b0770a7853d9b8eb0fb8b4f824db3d2b01c2741d
Thais Quintela <thais.s.quintela@gmail.com>
2021-04-25 23:51:41 -0300

    Updates

---
+++ precache-manifest.cb2340407f605ea7dec3b1e372899ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a727c1ec62bca8a371f0a56144b20c5",
+    "url": "/starwars-planet-search/index.html"
+  },
+  {
+    "revision": "ceae9db5a811c288b6e5",
+    "url": "/starwars-planet-search/static/css/main.c8e09488.chunk.css"
+  },
+  {
+    "revision": "9983cd9a48091b050261",
+    "url": "/starwars-planet-search/static/js/2.f1f99aec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planet-search/static/js/2.f1f99aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceae9db5a811c288b6e5",
+    "url": "/starwars-planet-search/static/js/main.0eb21837.chunk.js"
+  },
+  {
+    "revision": "7ae9ac9354f1d33a3553",
+    "url": "/starwars-planet-search/static/js/runtime-main.9d6297ae.js"
+  }
+]);
